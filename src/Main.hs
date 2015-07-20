0c8c53c52c24e0b2b44e5d48c1112d51bb761204
Alexander Mattes <alexm128@googlemail.com>
2015-07-20 19:35:38 +0200

    Add Main.hs

---
+++ src/Main.hs
@@ -0,0 +1,17 @@
+{-|
+    Module      : Main
+    Description : Main module with CLI
+    Copyright   : (C) Alexander Mattes, 2015
+    License     : MIT
+    Maintainer  : Alexander Mattes <alexm128@googlemail.com>
+    Stability   : experimental
+    Portability : portable
+
+    The Main module with all CLI related functions.
+-}
+module Main (main) where
+
+main :: IO ()
+main = do
+    putStrLn "~~~ Project Euler Solver ~~~"
+    putStrLn "Nothing solved yet."
