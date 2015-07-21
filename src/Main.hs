ac715d86a3d99ed79a92d7d311c0cf006f43c45f
Alexander Mattes <alexm128@googlemail.com>
2015-07-21 16:56:38 +0200

    Add basic CLI

--- src/Main.hs
+++ src/Main.hs
@@ -9,9 +9,56 @@
 
     The Main module with all CLI related functions.
 -}
-module Main (main) where
+module Main
+    ( main
+    ) where
 
+
+import System.IO (hFlush, stdout)
+import System.TimeIt (timeItT)
+import Data.Maybe (fromJust, isNothing)
+import Text.Printf (printf)
+
+import Solve (ProblemID, minProblemID, maxProblemID, solveProblem, solutions, numberSolvedProblems)
+import Verify (isCorrectFor)
+
+
+-- | The main function with the CLI.
 main :: IO ()
 main = do
     putStrLn "~~~ Project Euler Solver ~~~"
-    putStrLn "Nothing solved yet."
+    putStrLn (show numberSolvedProblems ++ " out of "
+              ++ show (maxProblemID-minProblemID+1) ++ " problems have solutions available.")
+    let loop = do
+            putStr "Please enter problem number (0 for all): "
+            hFlush stdout
+            n <- readLn :: IO ProblemID
+            if n==0 then do
+                printAllSolutions
+            else do
+                printSolution n
+            loop
+    loop
+
+-- | Prints all available solutions.
+printAllSolutions :: IO ()
+printAllSolutions = do
+    putStrLn "~~~ Gotta solve em all! ~~~"
+    mapM_ (printSolution . fst) $ solutions
+
+-- | Prints a solution for a given problems.
+--   Also checks if the solution is correct and how long the calculation took.
+printSolution :: ProblemID -> IO ()
+printSolution n = do
+    let a = solveProblem n
+    if isNothing a then do
+        putStrLn ("Problem " ++ show n ++ " not solved yet.")
+    else do
+        printf "Problem %3d: " n
+        let s = fromJust a
+        (t,_) <- timeItT  $ printf "%-35s" s
+        let c = case s `isCorrectFor` n of
+                Nothing    -> "   (unknown)"
+                Just True  -> "   (correct)"
+                Just False -> " (incorrect)"
+        printf "%s time: %6.2fs\n" c t
