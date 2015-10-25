e3099901ec637a1c4fc91dfdfe3fd361987fc1f1
Alexander Mattes <alexm128@googlemail.com>
2015-10-25 20:09:40 +0100

    Add data files for some problems

---
+++ src/ProblemData.hs
@@ -0,0 +1,47 @@
+{-# LANGUAGE QuasiQuotes #-}
+{-|
+    Module      : ProblemData
+    Description : Data n
+    Copyright   : (C) Alexander Mattes, 2015
+    License     : MIT
+    Maintainer  : Alexander Mattes <alexm128@googlemail.com>
+    Stability   : experimental
+    Portability : portable
+
+
+-}
+module ProblemData where
+
+
+import Utils (litFile)
+
+
+problem8 :: String
+problem8 = [litFile|src/problemData/Problem8.txt|]
+
+problem11 :: String
+problem11 = [litFile|src/problemData/Problem11.txt|]
+
+problem13 :: String
+problem13 = [litFile|src/problemData/Problem13.txt|]
+
+problem18 :: String
+problem18 = [litFile|src/problemData/Problem18.txt|]
+
+problem22 :: String
+problem22 = [litFile|src/problemData/Problem22.txt|]
+
+problem42 :: String
+problem42 = [litFile|src/problemData/Problem42.txt|]
+
+problem54 :: String
+problem54 = [litFile|src/problemData/Problem54.txt|]
+
+problem59 :: String
+problem59 = [litFile|src/problemData/Problem59.txt|]
+
+problem67 :: String
+problem67 = [litFile|src/problemData/Problem67.txt|]
+
+problem89 :: String
+problem89 = [litFile|src/problemData/Problem89.txt|]
