da8b56ae87224383136f5bb8adfc8679c7a6b750
Alexander Mattes <alexm128@googlemail.com>
2015-10-25 21:10:13 +0100

    Add solution for problems 1,2

--- src/Solutions/Solutions0XX.hs
+++ src/Solutions/Solutions0XX.hs
@@ -9,12 +9,15 @@ module Solutions.Solutions0XX
     ) where
 
 
+import Prelude hiding (sum, product)
 import Utils (ProblemID, Solution, js)
-
+import Solutions.Math
 
 -- | Solutions for problems with IDs between 1 and 99.
 solveProblem0XX :: ProblemID -> Maybe Solution
-solveProblem0XX 1 = js 123456789 -- Edit this line
-solveProblem0XX 2 = Just "Edit me!" -- Edit this line
--- Add further solutions here
+
+solveProblem0XX 1 = js $ sum [3,6..999] + sum [5,10..999] - sum [15,30..999]
+
+solveProblem0XX 2 = js . sum . takeWhile (<4000000) . filter even $ fibs
+
 solveProblem0XX _ = Nothing
